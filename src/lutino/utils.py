8c4cf2e9540d7e3dc166c0627eaf2ccd35d573b5
vahid <vahid.mardani@gmail.com>
2016-05-14 00:22:23 +0430

    deprecation decorator

--- src/lutino/utils.py
+++ src/lutino/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import importlib.util
 import sys
+import warnings
 from os.path import dirname, abspath
 __author__ = 'vahid'
 
@@ -23,3 +24,26 @@ def import_python_module_by_filename(name, module_filename):
     imported_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(imported_module)
     return imported_module
+
+
+def deprecated(func):
+    """
+    This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used.
+    """
+
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # Turn off filter
+        warnings.warn(
+            'Call to deprecated function %s.' % func.__name__,
+            category=DeprecationWarning,
+            stacklevel=2
+        )
+        warnings.simplefilter('default', DeprecationWarning)  # Reset filter
+        return func(*args, **kwargs)
+
+    new_func.__name__ = func.__name__
+    new_func.__doc__ = func.__doc__
+    new_func.__dict__.update(func.__dict__)
+    return new_func
