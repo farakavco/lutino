9d1dcbbbd16a16c0920611acf85559a3f27c4c9f
vahid <vahid.mardani@gmail.com>
2016-06-05 14:34:18 +0430

    singleton

--- src/lutino/tests/test_singleton.py
+++ src/lutino/tests/test_singleton.py
@@ -14,13 +14,25 @@ class TestSingletonMeta(unittest.TestCase):
             pass
 
         class MyClass(A, B, metaclass=SingletonMeta):
-
             def __init__(self):
                 nonlocal init_called
                 init_called += 1
 
         self.assertIs(MyClass(), MyClass())
         self.assertEqual(init_called, 2)
+
+    def test_singleton_meta_inheritance(self):
+
+        class A(object):
+            pass
+
+        class B(object, metaclass=SingletonMeta):
+            pass
+
+        class MyClass(A, B):
+            pass
+
+        self.assertIs(MyClass(), MyClass())
 
 
 if __name__ == '__main__':
