f8ba7f3e3cac1035a06ca158381e879da6aa660a
Aida Mirabadi <aida.mirabadi@gmail.com>
2016-08-06 13:10:10 +0430

    Rename VideoVisitTest to NumericalChachedFieldTest in tests

--- src/lutino/caching/tests/test_numerical_cached_field.py
+++ src/lutino/caching/tests/test_numerical_cached_field.py
@@ -1,6 +1,6 @@
 import unittest
 
-from lutino.caching.fields.numerical_cached_field import VideoVisitCachedField
+from lutino.caching.fields.numerical_cached_field import NumericalCachedField
 from lutino.caching.tests.base import RedisTestCase
 
 
@@ -9,21 +9,21 @@ class TestVideoVisitCachedField(RedisTestCase):
     def setUp(self):
         super().setUp()
         self.model_identity = 'AB'
-        self.video_visit_cached_field = VideoVisitCachedField(self.redis, self.model_identity)
+        self.numerical_cached_field = NumericalCachedField(self.redis, 'videos', self.model_identity, 'visits')
 
     def test_numerical_cached_field(self):
 
         default_visit = 10
-        self.video_visit_cached_field.set(default_visit)
-        stored_visit = self.video_visit_cached_field.get()
+        self.numerical_cached_field.set(default_visit)
+        stored_visit = self.numerical_cached_field.get()
         self.assertEqual(default_visit, stored_visit)
 
-        self.video_visit_cached_field.increment()
-        incremented_visit = self.video_visit_cached_field.get()
+        self.numerical_cached_field.increment()
+        incremented_visit = self.numerical_cached_field.get()
         self.assertEqual(stored_visit + 1, incremented_visit)
 
-        self.video_visit_cached_field.decrement()
-        decremented_visit = self.video_visit_cached_field.get()
+        self.numerical_cached_field.decrement()
+        decremented_visit = self.numerical_cached_field.get()
         self.assertEqual(stored_visit, decremented_visit)
 
 
